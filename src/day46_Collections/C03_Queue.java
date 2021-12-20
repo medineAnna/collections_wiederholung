fbe65573083f72681d723d1014fe2328aebf7d8f
medineannamammedova <medineannamammedova@Medine-MBP.fritz.box>
2021-12-20 22:07:37 +0100

    ilk

--- src/day46_Collections/C03_Queue.java
+++ src/day46_Collections/C03_Queue.java
@@ -6,14 +6,23 @@ import java.util.Queue;
 public class C03_Queue {
 
 	public static void main(String[] args) {
+		//gelen sona gelir,silinen bastan silinir
+		
 		
 		Queue<String> kuyruk=new LinkedList<>();//queu methodlari kullanmak icin data type queue dir
+		
+		
 		kuyruk.add("Lutfullah");
 		kuyruk.add("Mustafa");
 		kuyruk.add("Ridvan");
 		
 		System.out.println(kuyruk);
+		
+		kuyruk.remove();
+		System.out.println(kuyruk);
 
+		kuyruk.remove();
+		System.out.println(kuyruk);//sira ile siler
 	}
 
 }
---
+++ src/day46_Collections/C04_Queue.java
@@ -0,0 +1,18 @@
+package day46_Collections;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class C04_Queue {
+
+	public static void main(String[] args) {
+		
+		Deque<String> ciftBasli=new LinkedList<>();
+		
+		ciftBasli.add("Yildiz");
+		ciftBasli.add("Ali");
+		System.out.println(ciftBasli);//add() Queue'den geldigi icin sona ekler
+
+	}
+
+}
