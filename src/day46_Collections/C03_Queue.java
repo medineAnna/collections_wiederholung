ca681b04f97471b7f44a5558d0b9f7e0a31177da
medineannamammedova <medineannamammedova@Medine-MBP.fritz.box>
2021-12-20 21:29:24 +0100

    ilk

---
+++ src/day46_Collections/C03_Queue.java
@@ -0,0 +1,19 @@
+package day46_Collections;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class C03_Queue {
+
+	public static void main(String[] args) {
+		
+		Queue<String> kuyruk=new LinkedList<>();//queu methodlari kullanmak icin data type queue dir
+		kuyruk.add("Lutfullah");
+		kuyruk.add("Mustafa");
+		kuyruk.add("Ridvan");
+		
+		System.out.println(kuyruk);
+
+	}
+
+}
